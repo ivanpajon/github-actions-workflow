70cd85fddbbfdacb286425c44844e39bae6148fa
Ivan <ivangpajon@hotmail.com>
2024-03-26 20:39:41 +0100

    feature: Implement CI and CD

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
+// Name of our GitHub repository
+const GITHUB_REPO_NAME = 'github-actions-test';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  basePath: `/${GITHUB_REPO_NAME}`,
+  assetPrefix: `/${GITHUB_REPO_NAME}/`,
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  }
+};
 
 export default nextConfig;
